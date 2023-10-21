04232063919b16c7dd5188bcb27996e67ab1d0b1
audreysoper <74442056+audreysoper@users.noreply.github.com>
2023-10-21 15:07:16 -0400

    Fixed being dumb with git
    My dumb git behavior broke things so I'm fixing

--- src/gui/AppBoard.java
+++ src/gui/AppBoard.java
@@ -1,30 +1,18 @@
 package gui;
 
-<<<<<<< Updated upstream
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.MenuItem;
-import org.eclipse.swt.widgets.MessageBox;
-import org.eclipse.swt.widgets.Shell;
-=======
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.*;
->>>>>>> Stashed changes
 import org.eclipse.wb.swt.SWTResourceManager;
 
 import orgObjects.Category;
 import orgObjects.Question;
 
+import java.io.BufferedReader;
 import java.io.File;
-<<<<<<< Updated upstream
-=======
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
->>>>>>> Stashed changes
 import java.util.Scanner;
 import java.util.prefs.Preferences;
 
@@ -36,18 +24,6 @@ import org.eclipse.swt.layout.FillLayout;
 
 public class AppBoard {
 
-<<<<<<< Updated upstream
-	protected Shell shell;
-	private int boxH;
-	protected MenuItem saveMenuItem;
-	protected MenuItem openMenuItem;
-	protected MenuItem newMenuItem;
-	protected MenuItem mcToggleMenuItem;
-	protected MenuItem pathingMenuItem;
-	public static String homeFolder="";
-	public static Preferences userPrefs;
-	public static boolean advancedPathing;
-=======
 	protected static List<Shell> shells;
 
 
@@ -92,7 +68,6 @@ public class AppBoard {
 
 
 
->>>>>>> Stashed changes
 	/**
 	 * Launch the application.
 	 * 
@@ -100,10 +75,6 @@ public class AppBoard {
 	 */
 	public static void main(String[] args) {
 
-<<<<<<< Updated upstream
-		userPrefs = Preferences.userRoot(); 
-		advancedPathing=userPrefs.getBoolean("advancedPathing", false);
-=======
 		display = Display.getDefault();
 
 		userPrefs = Preferences.userRoot();
@@ -111,15 +82,10 @@ public class AppBoard {
 		localize = userPrefs.getBoolean("localize", false);
 		mcEnabled = userPrefs.getBoolean("mcEnabled", false);
 		shells= new ArrayList<>();
->>>>>>> Stashed changes
 		try {
 			//AppBoard window = new AppBoard();
 
-<<<<<<< Updated upstream
-			window.openBlank();
-=======
 			openBlank(false);
->>>>>>> Stashed changes
 		} catch (Exception e) {
 			e.printStackTrace();
 
@@ -131,39 +97,17 @@ public class AppBoard {
 	/**
 	 * Open the window.
 	 */
-<<<<<<< Updated upstream
-	public void openBlank() {
-=======
 	public static void openBlank(boolean mc) {
->>>>>>> Stashed changes
 
 		Shell shell=new Shell();
 		shells.add(shell);
 		shell.setLayout(new FillLayout());
 		shell.setText("Trivia Question Viewer");
-<<<<<<< Updated upstream
-		
-		
-		Display display = Display.getDefault();
-		boxH = (display.getBounds().height) / 7;
-		int shellW = (display.getBounds().width) / 3;
-
-		Board current =createContents(createBlank(), null);
-		Menu menuBar=createMenu(current);
-		shell.setMenuBar(menuBar);
-		shell.open();
-		// shell.layout();
-		// shell.pack();
-		Point size = shell.computeSize(SWT.DEFAULT, SWT.DEFAULT);
-		shell.setSize(size.x, size.y / 3 * 2);
-
-=======
 
 		Board current =new Board(shell,SWT.NONE,createBlank(mc),mc);
 		shell.setMenuBar(createMenu(shell,current));
 		shell.open();
 		shell.layout();
->>>>>>> Stashed changes
 		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
@@ -171,47 +115,6 @@ public class AppBoard {
 		}
 	}
 
-<<<<<<< Updated upstream
-	private Menu createMenu(Board curr) {
-		Menu bar= new Menu(shell,SWT.BAR);
-		MenuItem fileItem = new MenuItem (bar, SWT.CASCADE);
-		fileItem.setText ("&File");
-		Menu fileMenu = new Menu (shell, SWT.DROP_DOWN);
-		fileItem.setMenu (fileMenu);
-		saveMenuItem = new MenuItem (fileMenu, SWT.PUSH);
-		saveMenuItem.setData(curr.getShell());
-		saveMenuItem.addSelectionListener (curr.saveASAdapter);
-		saveMenuItem.setText ("Save\t Ctrl+S");
-		saveMenuItem.setAccelerator (SWT.MOD1 + 'S');
-		
-		
-		openMenuItem=new MenuItem (fileMenu, SWT.PUSH);
-		openMenuItem.setData(curr.getShell());
-		openMenuItem.addSelectionListener (curr.openBoardAdapter);
-		openMenuItem.setText ("Open\t Ctrl+O");
-		openMenuItem.setAccelerator (SWT.MOD1 + 'O');
-		
-		newMenuItem=new MenuItem (fileMenu, SWT.PUSH);
-		newMenuItem.setData(curr.getShell());
-		newMenuItem.addSelectionListener (curr.newBoardAdapter);
-		newMenuItem.setText ("New\t Ctrl+N");
-		newMenuItem.setAccelerator (SWT.MOD1 + 'N');
-		
-		pathingMenuItem = new MenuItem (fileMenu, SWT.CHECK);
-		pathingMenuItem.setData(curr.getShell());
-		pathingMenuItem.setText ("Toggle Advanced Pathing");
-		pathingMenuItem.setSelection(advancedPathing);
-		pathingMenuItem.addSelectionListener (new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				advancedPathing=!advancedPathing;
-				userPrefs.putBoolean("advancedPathing", advancedPathing); 
-				pathingMenuItem.setSelection(advancedPathing);
-				
-			
-			}
-		});
-=======
 	public static void redraw(){
 		for (Shell s:shells
 		) {
@@ -297,76 +200,17 @@ public class AppBoard {
 //
 //			}
 //		});
->>>>>>> Stashed changes
 		
+		
+
 		return bar;
 	}
 
-<<<<<<< Updated upstream
-	/*
-	 * FOR OPENING OTHER FILES AFTER INITIAL
-	 */
 
-	public void openFile(File source) {
-		shell = new Shell();
-		shell.setLayout(new FillLayout());
-		shell.setText("Trivia Question Viewer");
-		Display display = Display.getDefault();
-		boxH = (display.getBounds().height) / 7;
-
-		Category[] cats = parseBoard(source);
-		if (cats != null) {
-			Board openBoard = createContents(cats, source);
-			Menu menuBar=createMenu(openBoard);
-			shell.setMenuBar(menuBar);
-			shell.open();
-			Point size = shell.computeSize(SWT.DEFAULT, SWT.DEFAULT);
-			shell.setSize(size.x, size.y / 3 * 2);
-
-			while (!shell.isDisposed()) {
-				if (!display.readAndDispatch()) {
-					display.sleep();
-				}
-			}
-		} else {
-
-			shell.dispose();
-
-		}
-
-	}
-=======
-
->>>>>>> Stashed changes
 
 	/**
 	 * Create contents of the window.
 	 */
-<<<<<<< Updated upstream
-	protected Board createContents(Category[] cats, File source) {
-
-		// scrollContainer.setBackground(listBG);
-		/*
-		 * scrollContainer. addDisposeListener(new DisposeListener() {
-		 * 
-		 * @Override public void widgetDisposed(DisposeEvent e) { listBG.dispose(); }
-		 * });
-		 */
-		// Board myBoard = new Board(scrollContainer, SWT.NONE,createBlank());
-		// Board myBoard =new Board(scrollContainer, SWT.NONE,parseBoard(dir));
-		
-		if (source != null) {
-			boolean mC=cats[0].getQuestions()[0].getAnswer().split("\\^").length>2;
-			for(String a:cats[0].getQuestions()[0].getAnswer().split("\\^"))
-				System.out.println(a);
-			return new Board(shell, SWT.NONE, cats, source,mC);
-		}
-		return new Board(shell, SWT.NONE, cats);
-
-	}
-
-	private Category[] createBlank() {
-=======
 	public static void createContents(Category[] cats, File source) {
 
 		System.out.println(cats[0].getQuestions()[0].getAnswer().split("\\^").length);
@@ -402,17 +246,21 @@ public class AppBoard {
 	}
 
 	private static Category[] createBlank(boolean mc) {
->>>>>>> Stashed changes
 		Category[] cats = new Category[7];
 		Question[] qs;
 		for (int i = 0; i < cats.length; i++) {
 			qs = new Question[5];
 
 			for (int j = 0; j < qs.length; j++) {
-				qs[j] = new Question("", "", 'N', "text", "", j);
+				if(mc) {
+					qs[j] = new Question(" ", "^ ^ ^ ^", 'N', "text", "", j);
+				}else {
+					qs[j] = new Question(" ", "", 'N', "text", "", j);
+				}
+				
 			}
 			try {
-				cats[i] = new Category("", qs, i);
+				cats[i] = new Category(" ", qs, i);
 				cats[i].changeType("text");
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -424,102 +272,29 @@ public class AppBoard {
 	public static Category[] parseBoard(File source) {
 		Category[] cats = new Category[7];
 		try {
-			Scanner scanner = new Scanner(source);
+			Scanner scanner = new Scanner(new BufferedReader(new FileReader(source)));
 			String position = "newCat";
 			int qNum = 0;
 			int catNum = 0;
 			Category currentCat = null;
 			Question[] questions = new Question[5];
 			String text;
+			String question;
 			String answer;
 			String dd;
 			String format;
-			
+			String textStyle = "";
+
 			boolean sameType = true;
 
 			while (catNum < 6 && scanner.hasNextLine()) {
 				text = scanner.nextLine();
 				switch (position) {
 
-<<<<<<< Updated upstream
-				// WHEN POSITION IS A NEW CATEGORY
-				case "newCat":
-					if (text.length() > 1) {
-						currentCat = new Category(text.trim(), catNum);
-					} else if (text.length() > 0) {
-						currentCat = new Category(" ", catNum);
-					}
-					position = "question";
-					break;
-
-				// WHEN POSITION IS A NEW QUESTION
-				case "question":
-					try {
-						answer = scanner.nextLine();
-						dd = scanner.nextLine();
-						if(!(dd.contains("N")||dd.contains("Y"))) {
-							System.out.println(dd);
-							answer=answer+" "+dd;
-							dd=scanner.nextLine();
-						}
-						format = scanner.nextLine();
-						if (answer.endsWith("^^^^") && answer.length() > 4) {
-							answer = answer.substring(0, answer.lastIndexOf("^^^^"));
-						} else if (answer.length() <= 4) {
-							answer = " ";
-						}
-						
-						if (qNum > 0 && sameType) {
-							sameType = questions[qNum - 1].getTypeDetails().contains(format);
-						}
-
-						questions[qNum] = new Question(text.trim(), answer.trim(), dd.charAt(0), "", format, qNum);
-
-						qNum++;
-					} catch (Exception e1) {
-						// TODO Auto-generated catch block
-						Shell fuck=new Shell();
-						fuck.open();
-						Image sad=SWTResourceManager.getImage(AppBoard.class, "MEDowo.png");
-						fuck.setBackgroundImage(sad);
-						fuck.setSize(fuck.computeSize(sad.getBounds().width,sad.getBounds().height));
-						
-						MessageBox ope = new MessageBox(fuck, SWT.OK);
-						ope.setMessage("Hmmmmm.... This file doesn't look quite like I was expecting it to. "
-								+ "\n You sure this is a trivia file that's formatted properly?"
-								+ "\n If so please send it to Audrey to figure out why I can't read it");
-						ope.setText("File Read Error");
-						ope.open();
-						fuck.dispose();
-						e1.printStackTrace();
-						return null;
-					}
-					// if we've got all out questions, change back to category start
-					if (qNum > 4) {
-						currentCat.addQuestions(questions);
-
-						// set the type of the current category
-
-						// if the text is more than 4 characters THEN its safe to do the switch
-						// statement
-						if (sameType) {
-							switch (format.charAt(0)) {
-							case 'P':
-								currentCat.changeType("picture");
-								break;
-							case 'S':
-								currentCat.changeType("audio");
-								break;
-							default:
-								currentCat.changeType("text");
-								break;
-							}
-=======
 					// WHEN POSITION IS A NEW CATEGORY
 					case "newCat" -> {
 						if (text.length() > 1) {
 							currentCat = new Category(text.trim(), catNum);
->>>>>>> Stashed changes
 						} else {
 							currentCat = new Category(" ", catNum);
 						}
@@ -622,46 +397,25 @@ public class AppBoard {
 					answer = " ";
 				}
 
-				Question finQ = new Question(text.trim(), answer.trim(), 'N', "text", "", 0);
-				cats[catNum] = new Category("final", new Question[] { finQ }, 7);
-				if(scanner.hasNext("`")) {
-					homeFolder=scanner.nextLine();
-				}
 			} catch (Exception e1) {
 				System.out.println("I couln't make the Final Question");
-				e1.printStackTrace();
+				text = "I couldn't find a final";
+				answer = "so this is here instead";
 			}
+
+			Question finQ = new Question(text.trim(), answer.trim(), 'N', "text", textStyle, 0);
+			cats[catNum] = new Category("final", new Question[] { finQ }, 7);
+
 			scanner.close();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
-<<<<<<< Updated upstream
-			e.printStackTrace();
-=======
 			return parseErrorScreen(e,"what the fuck",cats);
 
->>>>>>> Stashed changes
 		}
 
 		return cats;
 	}
 
-<<<<<<< Updated upstream
-	public static Composite getSad() {
-		Shell fuck=new Shell();
-		fuck.open();
-		fuck.setBackgroundImage(null);
-		Composite sad = new Composite(fuck, SWT.NONE);
-		sad.setLayout(new FillLayout());
-		Label owoSticker = new Label(sad, SWT.NONE);
-		Image bigOwo = SWTResourceManager.getImage(AppBoard.class, "MEDowo.png");
-		owoSticker.setImage(bigOwo);
-		owoSticker.setBounds(bigOwo.getBounds());
-		// owoSticker.setText("Plz stop breaking me");
-		fuck.layout(true);
-		fuck.pack();
-		return sad;
-	}
-=======
 	public static Category[] parseErrorScreen(Exception e1, String text,Category[] cats){
 		boolean canRectify=Arrays.stream(cats).filter(e -> e != null).count()>1;
 		Shell fuck = new Shell();
@@ -751,7 +505,6 @@ public class AppBoard {
 		return null;
 	}
 
->>>>>>> Stashed changes
 }
 
 
